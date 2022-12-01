d3c38d841a6708000612d963680f0730d47f2d85
leksaaas <113454473+leksaaas@users.noreply.github.com>
2022-12-01 17:20:19 +0300

    hjvjjv

---
+++ HW3/linkedlist.py
@@ -0,0 +1,99 @@
+class Node:
+    def __init__(self, value,
+                 prev_pointer=None, next_pointer=None):
+        self.set_value(value)
+        self.set_prev(prev_pointer)
+        self.set_next(next_pointer)
+
+    def get_value(self):
+        return self._value
+
+    def get_next(self):
+        return self._next_pointer
+
+    def get_prev(self):
+        return self._prev_pointer
+
+    def set_value(self, value):
+        self._value = value
+
+    def set_prev(self, prev_pointer):
+        self._prev_pointer = prev_pointer
+
+    def set_next(self, next_pointer):
+        self._next_pointer = next_pointer
+
+    def __str__(self):
+        return str(self.get_value())
+
+class List:
+    def __init__(self, collections = None):
+        self._start_pointer = None
+        self._finish_pointer = None
+        self._length = 0
+        if isinstance(collections, int):
+            self.append(collections)
+        elif isinstance(collections, list):
+            for i in collections:
+                self.append(i)
+
+    def __len__(self):
+        return self._length
+
+    def append(self, value):
+        if self._length == 0:
+            self._start_pointer = Node(value)
+            self._finish_pointer = self._start_pointer
+            self._length = 1
+        else:
+            self._finish_pointer.set_next(Node(value,
+                                               self._finish_pointer))
+            self._finish_pointer = self._finish_pointer.get_next()
+            self._length += 1
+
+    def __getitem__(self, i):
+        if i < 0 or i >= self._length:
+            return False
+        curr_pointer = self._start_pointer
+        for j in range(i):
+            curr_pointer = curr_pointer.get_next()
+        return curr_pointer.get_value()
+
+    def __str__(self):
+        arr = []
+        for i in range(self._length):
+            arr.append(str(self[i]))
+        return "[" + ", ".join(arr) + "]"
+
+    def __add__(self, other):
+        for i in range(len(other)):
+            self.append(other[i])
+        return self
+
+    def __radd__(self, other):
+        for i in range(len(self)):
+            other.append(self[i])
+        return other
+
+    def __eq__(self, other):
+        for i in range(len(self)):
+            self[i] = other[i]
+        return self
+
+    def pop(self, i):
+        l = List()
+        result = self[i]
+        for j in range(len(self)-1):
+            if j < i:
+                l.append(self[j])
+            if j >= i:
+                l.append(self[j+1])
+        self = l
+        return result
+
+A = List()
+for i in range(5):
+    A.append(i)
+for element in A:
+    print(element)
+print(A)
