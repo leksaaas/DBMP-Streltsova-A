ea387de841015a1c5e1c69f3561f573c56fadf00
leksaaas <113454473+leksaaas@users.noreply.github.com>
2026-03-02 13:20:40 +0300

    Update distribution.py

--- distribution.py
+++ distribution.py
@@ -1,12 +1,8 @@
 import numpy as np
 
-class LaplaceDistribution:    
+class LaplaceDistribution:
     @staticmethod
     def mean_abs_deviation_from_median(x: np.ndarray):
-        '''
-        Args:
-        - x: A numpy array of shape (n_objects, n_features)
-        '''
         ####
         # Do not change the class outside of this block
         median = np.median(x, axis=0)
@@ -14,14 +10,8 @@ class LaplaceDistribution:
         ####
 
     def __init__(self, features):
-        '''
-        Args:
-            features: array of shape (n_objects,) or (n_objects, n_features)
-        '''
         ####
         # Do not change the class outside of this block
-
-        # make 2D if 1D was passed
         features = np.asarray(features)
         if features.ndim == 1:
             features = features[:, None]
@@ -30,12 +20,7 @@ class LaplaceDistribution:
         self.scale = self.mean_abs_deviation_from_median(features)
         ####
 
-
     def logpdf(self, values):
-        '''
-        Args:
-            values: shape (n_objects,) or (n_objects, n_features)
-        '''
         ####
         # Do not change the class outside of this block
         values = np.asarray(values)
@@ -44,10 +29,6 @@ class LaplaceDistribution:
 
         return -np.log(2 * self.scale) - np.abs(values - self.loc) / self.scale
         ####
-        
-    
+
     def pdf(self, values):
-        '''
-        Returns probability density
-        '''
         return np.exp(self.logpdf(values))
